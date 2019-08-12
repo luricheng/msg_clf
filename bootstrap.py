01f7c4020a6dfbe08bfbb558d96005fb28b6f617
luricheng <707279218@qq.com>
2019-08-13 00:17:02 +0800

    pca&random_forest&svm&sgd

--- bootstrap.py
+++ bootstrap.py
@@ -1,9 +1,29 @@
 import pandas as pd
 import utils
-from model import tfidf, clfs
 import numpy as np
 from sklearn import metrics
 import time
+from sklearn.decomposition import PCA
+from Tfidf import Tfidf
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
+
+# todo Label==1的样本 数量过少
+# todo 后续: 过采样???
+
+transform_list = [
+    Tfidf(),
+    PCA(n_components=100),
+]
+
+models = [
+    XGBClassifier(random_state=1, n_estimators=100, n_jobs=4),
+    RandomForestClassifier(random_state=1, n_estimators=100, n_jobs=4),
+    SVC(random_state=1, kernel='rbf', class_weight='balanced'),
+    SGDClassifier(random_state=1, n_jobs=4)
+]
 
 if __name__ == '__main__':
     csv_data = pd.read_csv('./data/train.csv', usecols=[0, 1])
@@ -14,16 +34,21 @@ if __name__ == '__main__':
     train_csv, test_csv = utils.split_csv2train_test(csv_data, train_op=0.7)
     print("train_set: %s, test_set: %s" % (len(train_csv), len(test_csv)))
 
-    for model_name, clf_model in clfs.models.items():
-        start_time = time.time()
-        x_train, y_train = train_csv.Text.values, train_csv.Label.values
-        tfidf_model, tfidf_matrix = tfidf.train(x_train)
-        clf_model.fit(tfidf_matrix, y_train)
+    x_train, y_train = train_csv.Text.values, train_csv.Label.values.astype(int)
+    x_test, y_test = test_csv.Text.values, test_csv.Label.values.astype(int)
 
-        x_test, y_test = test_csv.Text.values, test_csv.Label.values
-        y_pred = clf_model.predict(tfidf_model.transform(x_test))
+    x_train = utils.fit_transform_all(transform_list, x_train)
+    x_test = utils.transform_all(transform_list, x_test)
+
+    for model in models:
+        start_time = time.time()
+        model.fit(x_train, y_train)
+        y_pred = model.predict(x_test)
         acc = np.sum(y_pred == y_test) / len(y_test)
-        print("model: %s\nacc: %s" % (model_name, acc))
-        print(metrics.classification_report(y_pred=y_pred.astype(int), y_true=y_test.astype(int), target_names=['0', '1']))
-        print('cost time: %s' % (time.time() - start_time))
+        print("====> model: %s" % utils.get_class_name(model))
+        print("====> acc: %s" % acc)
+        print('====> cost time: %s' % (time.time() - start_time))
+        print(metrics.classification_report(y_pred=y_pred.astype(int),
+                                            y_true=y_test.astype(int),
+                                            target_names=['0', '1']))
 
--- utils.py
+++ utils.py
@@ -3,3 +3,24 @@ def split_csv2train_test(csv, train_op):
     shuffle_csv = csv.sample(frac=1.0, random_state=1)
     k = int(len(csv) * train_op)
     return shuffle_csv[: k], shuffle_csv[k:]
+
+
+def __call_all(obj_list, func_name, x):
+    for o in obj_list:
+        if hasattr(o, func_name):
+            x = getattr(o, func_name)(x)
+        else:
+            raise "obj: %s has not func: %s" % (o, func_name)
+    return x
+
+
+def fit_transform_all(transform_list, x):
+    return __call_all(transform_list, "fit_transform", x)
+
+
+def transform_all(transform_list, x):
+    return __call_all(transform_list, "transform", x)
+
+
+def get_class_name(model):
+    return model.__class__.__name__
